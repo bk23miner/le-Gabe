aa70e9e463209d4f4ad095245e0d95403f5b315b
Bk23miner <bk23miner@gmail.com>
2026-08-29 00:04:13 +0200

    Upload-by-Bk

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,27 +2,37 @@ class_name Player extends CharacterBody2D
 
 @export var speed = 120
 @export var jump_speed = -180
-@export var gravity = 400
-
-@export var remaining_djumps = 1000
-@export var remaining_dash = 3
+@export var gravity = 500
+@export var remaining_djumps = 1
+@export var remaining_dash = 1
 @export var remaining_walljump = 0
+@export var Wings: Node2D#
+var dashing = false
+
+func _ready() -> void:
+	Wings = $Sprite2D
+	Wings.visible = false
 
 func _physics_process(delta):
 	# Add gravity every frame
-	velocity.y += gravity * delta
 
 	# Input affects x axis only
 	velocity.x = Input.get_axis("left", "right") * speed
+	var y_dir = Input.get_axis("up", "down")
 	
-	move_and_slide()
+
 
 	# Only allow jumping when on the ground or if djumps greater 0
 	if Input.is_action_just_pressed("jump") and !is_on_floor() and remaining_djumps>0:
+		Wings.visible = true
+		$Timer.start()
 		velocity.y = jump_speed
 		remaining_djumps -= 1
 	elif Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
+	elif Input.is_action_just_pressed("dash") && !dashing && remaining_dash > 0 :
+		$"Dash-Timer".start()
+		dashing = true
 	var Body = $AnimatedSprite2D
 	
 	var direction = sign(velocity.x)
@@ -30,10 +40,27 @@ func _physics_process(delta):
 	if direction > 0:
 		Body.play("right")
 	#links
-	if direction < 0:
+	elif direction < 0:
 		Body.play("left")	
 	elif direction == 0:
 		Body.play("idle")
-	if Input.is_action_just_pressed("dash"):
-		velocity = Input.get_vector("left", "right","up","down") * speed *3
 		
+	if dashing:
+		velocity.x =  Input.get_axis("left", "right") * (speed + 200)
+		velocity.y = y_dir * (speed+100)
+	else:	
+		velocity.y += gravity * delta
+	
+	
+	
+
+	move_and_slide()
+		
+func reset_Wings() -> void:
+	Wings.visible = false 
+	
+	
+
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false
