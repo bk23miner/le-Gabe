2d47bb39bcb32ac8b423ab99043b11ff94ab8a66
Bk23miner <bk23miner@gmail.com>
2026-08-28 20:20:02 +0200

    Proj-Conf

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,26 @@
-extends Node2D
+class_name Player extends CharacterBody2D
+
+@export var SPEED := 200.0
+@export var DASH := 300.0
+
+var dashing = false
+var cooldown = true
+var last_direction := "front"
+var is_picking_up = false
+var was_walking = false
+var disable_input = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	position.x = 100
-	position.y = 100
+func _physics_process(delta):
+	if disable_input:
+		return
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	#if Input.is_action_just_pressed("dash") and cooldown:
+	#	dashing = true
+	#	cooldown = false
+	#if dashing:
+	#	velocity = input_direction * (SPEED + DASH)
+	#else:
+	velocity = input_direction * SPEED
+	move_and_slide()
