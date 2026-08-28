3acef660cdb17bf43c34a6e015bd447db67f9419
Bk23miner <bk23miner@gmail.com>
2026-08-28 19:49:59 +0200

    Proj-Conf

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
-
+var PL: Node = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+
+
+	var scene = load("res://scenes/Playyer.tscn")
+	var instance = scene.instantiate()
+	add_child(instance)
+
+
+	PL = instance
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
