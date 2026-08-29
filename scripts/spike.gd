375fb73550c016b30dc5c83cab7afe1cf235b0c2
Bk23miner <bk23miner@gmail.com>
2026-08-29 23:19:01 +0200

    Upload-by-Bk

--- scripts/spike.gd
+++ scripts/spike.gd
@@ -1,9 +1,14 @@
 extends Node2D
+@export var frame = 1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if frame == 2:
+		$Area2D/AnimatedSprite2D.play("new_animation")
+	if frame == 3:
+		$Area2D/AnimatedSprite2D.play("new_animation1")
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
