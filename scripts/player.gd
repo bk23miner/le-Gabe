9e7588359195aab452c8a72bdde129cf25b7d787
Pale <pale@palenet.de>
2026-08-29 10:59:51 +0200

    death function

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,17 @@
 class_name Player extends CharacterBody2D
 
 @export var speed = 120
-@export var dash_speed = 200
+@export var dash_speed = 250
 @export var jump_speed = -180
 @export var gravity = 500
+
 @export var remaining_djumps = 1
 @export var remaining_dash = 1
 @export var remaining_walljump = 0
 @export var Wings: Node2D#
+
+var is_dead = false
+
 var f_dash = false
 var dashing = false
 var temp_x_vel
@@ -22,64 +26,76 @@ func _physics_process(delta):
 	#if f_dash:
 	#	f_dash = false
 	#	velocity.y= 0
+	if Input.is_action_pressed("kill"):
+		_death()
+	if is_dead == false :
+		if Input.is_action_just_pressed("dash") && !dashing && remaining_dash > 0 :
+			$"Dash-Timer".start()
+			dashing = true
+			temp_bd_y_vel = velocity.y
+			temp_x_vel = Input.get_axis("left", "right") *(speed + dash_speed)
+			temp_y_vel = Input.get_axis("up","down") * (speed + dash_speed)
+		if dashing:
+			velocity.x = temp_x_vel
+			velocity.y = temp_y_vel
+		elif !dashing:
 		
-	if Input.is_action_just_pressed("dash") && !dashing && remaining_dash > 0 :
-		$"Dash-Timer".start()
-		dashing = true
-		temp_bd_y_vel = velocity.y
-		temp_x_vel = Input.get_axis("left", "right") *(speed + dash_speed)
-		temp_y_vel = Input.get_axis("up","down") * (speed + dash_speed)
-	if dashing:
-		velocity.x = temp_x_vel
-		velocity.y = temp_y_vel
-	elif !dashing:
-		print("Grav")	
 		
-		velocity.y += gravity * delta
-		velocity.x = Input.get_axis("left", "right") * speed
-		var y_dir = Input.get_axis("up", "down")
+			velocity.y += gravity * delta
+			velocity.x = Input.get_axis("left", "right") * speed
+			var y_dir = Input.get_axis("up", "down")
 	
 
 
-		if Input.is_action_just_pressed("jump") and !is_on_floor() and remaining_djumps>0:
-			Wings.visible = true
-			$Timer.start()
-			velocity.y = jump_speed
-			remaining_djumps -= 1
-		elif Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = jump_speed
+			if Input.is_action_just_pressed("jump") and !is_on_floor() and remaining_djumps>0:
+				Wings.visible = true
+				$Timer.start()
+				velocity.y = jump_speed
+				remaining_djumps -= 1
+			elif Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = jump_speed
 
-	var Body = $AnimatedSprite2D
+		var Body = $AnimatedSprite2D
 	
-	var direction = sign(velocity.x)
+		var direction = sign(velocity.x)
 	#rechts	
-	if direction > 0:
-		Body.play("right")
+		if direction > 0:
+			Body.play("right")
 		#links
-	elif direction < 0:
-		Body.play("left")	
-	elif direction == 0:
-		Body.play("idle")
-		
+		elif direction < 0:
+			Body.play("left")	
+		elif direction == 0:
+			Body.play("idle")
 	
 	
 	
 	
 	
-	print(str(velocity.y))
+	# print(str(velocity.y))
 
-	move_and_slide()
+		move_and_slide()
 		
 func reset_Wings() -> void:
 	Wings.visible = false 
 	$Timer.stop()
+	$CollisionShape2D.disabled = true
 	
 	
 
 
 func _on_dash_timer_timeout() -> void:
 	f_dash = true
+	gravity = 0
 	dashing = false
 	print("finish dash")
 	$"Dash-Timer".stop()
-	velocity.y = 0
+	velocity.y  = -100
+	gravity = 500
+	print(str(PI))
+	
+func _death() -> void:
+	is_dead = true
+	var Body = $AnimatedSprite2D
+	Body.play("death")
+	
+	
